fb777bfcf415026de8780206a795507260ef8963
Stefanus Anggara <anggara.stefanus@gmail.com>
2015-03-07 12:15:38 +0700

    step3
    step3

--- android/AndroidSQLite1/app/src/main/java/com/stef_developer/androidsqlite1/MainActivity.java
+++ android/AndroidSQLite1/app/src/main/java/com/stef_developer/androidsqlite1/MainActivity.java
@@ -1,17 +1,40 @@
 package com.stef_developer.androidsqlite1;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        /**
+         * CRUD Operations
+         * */
+        // Inserting Contacts
+        db.addContact(new Contact("Ravi", "9100000000"));
+        db.addContact(new Contact("Srinivas", "9199999999"));
+        db.addContact(new Contact("Tommy", "9522222222"));
+        db.addContact(new Contact("Karthik", "9533333333"));
+
+        ListView lv = (ListView) findViewById(R.id.myLV);
+
+        List<Contact> contacts = db.getAllContacts();
+        ArrayAdapter<Contact> adapter = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1, contacts);
+
+        lv.setAdapter(adapter);
     }
 
 
