ca6593547e66a274ba51654b4721a6e83a31d8f6
Stefanus Anggara <anggara.stefanus@gmail.com>
2015-03-09 14:27:29 +0700

    step3b
    step3b

---
+++ android/AndroidSQLite1/app/src/main/java/com/stef_developer/androidsqlite1/model/Tag.java
@@ -0,0 +1,42 @@
+package com.stef_developer.androidsqlite1.model;
+
+/**
+ * Created by Stefanus Anggara on 07/03/2015.
+ */
+public class Tag
+{
+    int id;
+    String tag_name;
+
+    // constructors
+    public Tag() {
+
+    }
+
+    public Tag(String tag_name) {
+        this.tag_name = tag_name;
+    }
+
+    public Tag(int id, String tag_name) {
+        this.id = id;
+        this.tag_name = tag_name;
+    }
+
+    // setter
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTagName(String tag_name) {
+        this.tag_name = tag_name;
+    }
+
+    // getter
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTagName() {
+        return this.tag_name;
+    }
+}
