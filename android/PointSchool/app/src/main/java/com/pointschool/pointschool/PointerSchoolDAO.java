70c25cb0ccbeecd419e95007d5d89e2fe5201b72
Raga <raga.krilido@gmail.com>
2015-03-15 16:55:19 +0700

    Some debug for PointerSchoolDBDAO

--- android/PointSchool/app/src/main/java/com/pointschool/pointschool/PointerSchoolDAO.java
+++ android/PointSchool/app/src/main/java/com/pointschool/pointschool/PointerSchoolDAO.java
@@ -2,8 +2,8 @@ package com.pointschool.pointschool;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-
 import com.pointschool.pointschool.database.DatabaseHandler;
+import java.sql.SQLException;
 
 /**
  * Created by Krilido on 3/15/2015.
@@ -13,13 +13,17 @@ public class PointerSchoolDAO {
     private DatabaseHandler dbHelper;
     private Context mContext;
 
-    public PointerSchoolDAO (Context context){
+    public PointerSchoolDAO (Context context) throws SQLException {
         this.mContext = context;
         dbHelper = DatabaseHandler.getHelper(mContext);
-        open();
+        try {
+            open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void open() throws SecurityException{
+    public void open() throws SQLException {
         if (dbHelper == null)
             dbHelper = DatabaseHandler.getHelper(mContext);
         database = dbHelper.getWritableDatabase();
