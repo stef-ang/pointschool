05d24ade2464a7a86c7a5cf2987ca688836760de
Stefanus Anggara <anggara.stefanus@gmail.com>
2015-04-16 19:12:53 +0700

    Update Layout
    List Mapel SMA

--- android/PointerD/PointerB/app/src/main/java/com/example/penisriwahyu/pointerb/SubActivitySMA.java
+++ android/PointerD/PointerB/app/src/main/java/com/example/penisriwahyu/pointerb/SubActivitySMA.java
@@ -1,6 +1,7 @@
 package com.example.penisriwahyu.pointerb;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,22 +9,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class SubActivitySMA extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sub_activity_sma);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "ITCKRIST.ttf");
+
         Toolbar toolbar= (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-        Button btnMat  = (Button) findViewById(R.id.sma_mat);
+
+        TextView tvHeading = (TextView) findViewById(R.id.teksMapel);
+        tvHeading.setTypeface(font);
+
+        Button btnMat  = (Button) findViewById(R.id.sma_mat_ipa);
         Button btnBio  = (Button) findViewById(R.id.sma_biologi);
         Button btnFis  = (Button) findViewById(R.id.sma_fisika);
         Button btnKim  = (Button) findViewById(R.id.sma_kimia);
         Button btnEko  = (Button) findViewById(R.id.sma_ekonomi);
-        Button btnSosio  = (Button) findViewById(R.id.sma_sosiologi);
         Button btnBind = (Button) findViewById(R.id.sma_bind);
         Button btnBing = (Button) findViewById(R.id.sma_bing);
 
@@ -77,16 +86,6 @@ public class SubActivitySMA extends ActionBarActivity {
                 startActivity(i);
             }
         });
-        btnSosio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(SubActivitySMA.this, smaSlide.class);
-                Bundle arg = new Bundle();
-                arg.putInt("NoMapel",5);
-                i.putExtras(arg);
-                startActivity(i);
-            }
-        });
         btnBind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -110,25 +109,25 @@ public class SubActivitySMA extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.men_sub_activity_sma, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.men_sub_activity_sma, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
